8b87259cbcc13939c1c0135371a37a2b84a6814a
6davidd <planadavid06@gmail.com>
2026-03-31 01:24:57 +0200

    20:00-21:30 arasi

--- lib/slots.ts
+++ lib/slots.ts
@@ -2,6 +2,7 @@ export const WEEKDAY_SLOTS = [
   { start: "15:30", end: "17:00" },
   { start: "17:00", end: "18:30" },
   { start: "18:30", end: "20:00" },
+  { start: "20:00", end: "21:30" },
 ];
 
 export const SATURDAY_SLOTS = [
