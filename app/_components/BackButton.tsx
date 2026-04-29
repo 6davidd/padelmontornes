de9bece1462db369e1f151fea2b420c41f9fa5f1
6davidd <planadavid06@gmail.com>
2026-04-29 07:48:37 +0200

    flecha

--- app/_components/BackButton.tsx
+++ app/_components/BackButton.tsx
@@ -64,7 +64,7 @@ export default function BackButton({
       <svg
         aria-hidden="true"
         viewBox="0 0 24 24"
-        className="-translate-y-px h-6 w-6"
+        className="-translate-y-0.5 h-6 w-6"
         fill="none"
         stroke="currentColor"
         strokeWidth="2.25"
